4e53bd8fcd3bff5b0ac40c28ac33a6550db548ed
beatka <beata.silwanowicz@gmail.com>
2023-04-05 23:08:46 +0200

    remove unnecessary jquery reference

--- cypress/e2e/TS1.cy.js
+++ cypress/e2e/TS1.cy.js
@@ -2,7 +2,6 @@ describe('my first scenario', () => {
 
     it('visit main page and click top menu button', () => {
 
-        //const positionY = jQuery('[data-id="4ee1fd56"]').offset().top
         cy.visit('http://fabrykatestow.pl')
         cy.get('a[href="https://fabrykatestow.pl/cypress"]').eq(1).click()
         cy.url().should('contain','https://fabrykatestow.pl/cypress/')
